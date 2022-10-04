3c20b44c7f4b66e2aff4a2d080e9e46112da6a29
Ogaga <iyaraogaga69@gmail.com>
2022-10-04 09:48:25 +0100

    Add class show to container app

--- script.js
+++ script.js
@@ -72,3 +72,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /// //////////////////////////////////////////////
+
+containerApp.classList.add('show');
+
+
